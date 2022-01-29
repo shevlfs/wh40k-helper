b4084173232b9af28d64d932e47e594063ce56bf
shevlfs <shevl.fs@gmail.com>
2022-01-29 23:49:11 +0300

    initial commit

---
+++ armybuilder/armybuilder/armybuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  armybuilderApp.swift
+//  armybuilder
+//
+//  Created by ted on 29.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct armybuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
