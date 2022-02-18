97027696c95b41c86558ca2d7089498f5849d657
shevlfs <shevl.fs@gmail.com>
2022-02-18 17:48:59 +0300

    fix

--- armybuilder/armybuilder/ContentView.swift
+++ armybuilder/armybuilder/ContentView.swift
@@ -9,10 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(){
+            VStack(alignment: .leading) {
+                Text("Army 1")
+                .font(.title)
+                HStack{
+                    Text("Faction")
+                        .font(.headline)
+                    Spacer()
+                    Text("1000 pts")
+                    .font(.subheadline)
+                }
+        
+            Spacer()
+        
+            }
+            
+        }
+        .padding(.all, 22.0)
+        }
     }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
