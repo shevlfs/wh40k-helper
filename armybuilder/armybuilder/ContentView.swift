dcc3def9b9100f939d4f2e6b384da1d23e2c86b4
shevlfs <60010221+shevlfs@users.noreply.github.com>
2022-03-25 19:47:51 +0300

    Revert ""
    This reverts commit 3b7477e2ebe6436cc527f12f325d4e1db963e305.

--- armybuilder/armybuilder/ContentView.swift
+++ armybuilder/armybuilder/ContentView.swift
@@ -22,11 +22,6 @@ struct ContentView: View {
             }.navigationTitle("Your armies").toolbar {
                 ToolbarItemGroup(placement: .primaryAction){
                     Button(action: {
-                        let army = Army(context: moc)
-                        army.troops = [:]
-                        army.id = UUID()
-                        army.factionID = 1
-                        try! moc.save()
                         self.showAddDialog.toggle()
                     }) {
                         Label("Add",systemImage:"plus.app")}.sheet(isPresented:$showAddDialog){
