382f4d3f038b522e39d8f2ac3af295c44ae26ffa
shevlfs <shevl.fs@gmail.com>
2022-06-21 20:53:03 +0300

    ...

--- armybuilder/armybuilder/unitStats.swift
+++ armybuilder/armybuilder/unitStats.swift
@@ -14,20 +14,31 @@ struct unitStats: View {
             Group{
                 VStack{
                     HStack{
-                        Text("M").fontWeight(.bold).foregroundColor(.white).padding(.leading)
-                        Text("WS").fontWeight(.bold).foregroundColor(.white)
-                        Text("BS").fontWeight(.bold).foregroundColor(.white)
-                        Text("S").fontWeight(.bold).foregroundColor(.white)
-                        Text("T").fontWeight(.bold).foregroundColor(.white)
-                        Text("W").fontWeight(.bold).foregroundColor(.white)
-                        Text("A").fontWeight(.bold).foregroundColor(.white)
-                        Text("Ld").fontWeight(.bold).foregroundColor(.white)
-                        Text("Sv").fontWeight(.bold).foregroundColor(.white).padding(.trailing)
-                        
-                    }.padding(.vertical)
+                        Group{
+                        Text("M").fontWeight(.bold).foregroundColor(.white).padding(4)
+                        Text("WS").fontWeight(.bold).foregroundColor(.white).padding(4)
+                        Text("BS").fontWeight(.bold).foregroundColor(.white).padding(4)
+                        Text("S").fontWeight(.bold).foregroundColor(.white).padding(4)
+                        Text("T").fontWeight(.bold).foregroundColor(.white).padding(4)
+                        Text("W").fontWeight(.bold).foregroundColor(.white).padding(4)
+                        Text("A").fontWeight(.bold).foregroundColor(.white).padding(4)
+                        Text("Ld").fontWeight(.bold).foregroundColor(.white).padding(4)
+                        Text("Sv").fontWeight(.bold).foregroundColor(.white).padding(4)
+                        }
+                    }
                     HStack{
-                        
-                    }.padding(.vertical)
+                        Group{
+                            Text("\(Unit.m)").fontWeight(.bold).foregroundColor(.white).padding(4)
+                        Text("\(Unit.ws)").fontWeight(.bold).foregroundColor(.white).padding(4)
+                        Text("\(Unit.bs)").fontWeight(.bold).foregroundColor(.white).padding(4)
+                        Text("\(Unit.s)").fontWeight(.bold).foregroundColor(.white).padding(4)
+                        Text("\(Unit.t)").fontWeight(.bold).foregroundColor(.white).padding(4)
+                        Text("\(Unit.w)").fontWeight(.bold).foregroundColor(.white).padding(4)
+                        Text("\(Unit.a)").fontWeight(.bold).foregroundColor(.white).padding(4)
+                        Text("\(Unit.ld)").fontWeight(.bold).foregroundColor(.white).padding(4)
+                        Text("\(Unit.sv)").fontWeight(.bold).foregroundColor(.white).padding(4)
+                        }
+                    }
                 }
                 
                 
