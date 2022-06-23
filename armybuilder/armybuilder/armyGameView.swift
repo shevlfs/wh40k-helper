847e834f03c2ec276b83200c53081da8050279de
shevlfs <shevl.fs@gmail.com>
2022-06-23 16:25:46 +0300

    tf

--- armybuilder/armybuilder/armyGameView.swift
+++ armybuilder/armybuilder/armyGameView.swift
@@ -12,8 +12,23 @@ struct armyGameView: View {
     @EnvironmentObject var armyControl: armyController
     var body: some View {
         VStack{
+            HStack{
+            HStack{
+                Text("M").font(.title2).fontWeight(.bold).foregroundColor(.white).padding()
+                Text("WS").font(.title2).fontWeight(.bold).foregroundColor(.white).padding()
+                Text("BS").font(.title2).fontWeight(.bold).foregroundColor(.white).padding()
+                Text("S").font(.title2).fontWeight(.bold).foregroundColor(.white).padding()
+                Text("T").font(.title2).fontWeight(.bold).foregroundColor(.white).padding()
+                Text("W").font(.title2).fontWeight(.bold).foregroundColor(.white).padding()
+                Text("A").font(.title2).fontWeight(.bold).foregroundColor(.white).padding()
+                Text("Ld").font(.title2).fontWeight(.bold).foregroundColor(.white).padding()
+                Text("Sv").font(.title2).fontWeight(.bold).foregroundColor(.white).padding()
+            }.background(RoundedRectangle(cornerRadius: 15).fill(.green)).frame(maxWidth: .infinity, alignment: .leading)
+            }
+            ScrollView{
             ForEach(armyControl.getTroops(armyID: armyID+1)){
-                troop in unitGameView(id: troop.unitid, factionID: armyControl.armies[armyID].factionID)
+                troop in unitGameView(id: troop.unitid-1, factionID: armyControl.armies[armyID].factionID).frame(maxWidth: .infinity, alignment: .leading)
+            }
             }
         }
     }
--- armybuilder/armybuilder/unitGameView.swift
+++ armybuilder/armybuilder/unitGameView.swift
@@ -11,13 +11,29 @@ struct unitGameView: View {
     @State var id: Int
     @State var factionID: Int
     var body: some View {
-        Text("\(globalstats[factionID].units[id].name)")
+        HStack{
+        HStack{
+            Text("\(globalstats[factionID].units[id].name)").fontWeight(.bold).multilineTextAlignment(.center).foregroundColor(.white).font(.title2).padding(4)
+            Group{
+            Text("\(globalstats[factionID].units[id].m)").font(.title2).fontWeight(.bold).foregroundColor(.white).padding()
+            Text("\(globalstats[factionID].units[id].ws)").font(.title2).fontWeight(.bold).foregroundColor(.white).padding()
+            Text("\(globalstats[factionID].units[id].bs)").font(.title2).fontWeight(.bold).foregroundColor(.white).padding()
+            Text("\(globalstats[factionID].units[id].s)").font(.title2).fontWeight(.bold).foregroundColor(.white).padding()
+            Text("\(globalstats[factionID].units[id].t)").font(.title2).fontWeight(.bold).foregroundColor(.white).padding()
+            Text("\(globalstats[factionID].units[id].w)").font(.title2).fontWeight(.bold).foregroundColor(.white).padding()
+            Text("\(globalstats[factionID].units[id].a)").font(.title2).fontWeight(.bold).foregroundColor(.white).padding()
+            Text("\(globalstats[factionID].units[id].ld)").font(.title2).fontWeight(.bold).foregroundColor(.white).padding()
+            Text("\(globalstats[factionID].units[id].sv)").font(.title2).fontWeight(.bold).foregroundColor(.white).padding()
+            }
+
+        }.padding()
+        }.background(RoundedRectangle(cornerRadius: 10).fill(.green)).padding()
     }
 }
 
 struct unitGameView_Previews: PreviewProvider {
     static var previews: some View {
-        unitGameView(id: 0, factionID: 0)
+        unitGameView(id: 20, factionID: 0)
             .previewInterfaceOrientation(.landscapeLeft)
     }
 }
