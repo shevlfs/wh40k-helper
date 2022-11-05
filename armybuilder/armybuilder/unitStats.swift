fe50db597e9b6cdb5eb9baffd39d1689d934e6d3
shevlfs <shevl.fs@gmail.com>
2022-11-05 23:19:09 +0300

    unitstats fix on small devices

--- armybuilder/armybuilder/unitStats.swift
+++ armybuilder/armybuilder/unitStats.swift
@@ -14,48 +14,48 @@ struct unitStats: View {
       Group {
         HStack {
           VStack {
-            Text("M").font(.title2).fontWeight(.bold).foregroundColor(.white).padding(4)
-              Text("\(Unit.m)".suffix(2)).font(.title2).fontWeight(.bold).foregroundColor(.white).padding(4)
+              Text("M").font(.title2).fontWeight(.bold).lineLimit(1).foregroundColor(.white).padding(4).minimumScaleFactor(0.01)
+              Text("\(Unit.m)".suffix(2)).font(.title2).fontWeight(.bold).lineLimit(1).foregroundColor(.white).padding(4).minimumScaleFactor(0.01)
 
           }
           VStack {
-            Text("WS").font(.title2).fontWeight(.bold).foregroundColor(.white).padding(4)
-            Text("\(Unit.ws)").font(.title2).fontWeight(.bold).foregroundColor(.white).padding(4)
+            Text("WS").font(.title2).fontWeight(.bold).lineLimit(1).foregroundColor(.white).padding(4).minimumScaleFactor(0.01)
+            Text("\(Unit.ws)").font(.title2).fontWeight(.bold).lineLimit(1).foregroundColor(.white).padding(4).minimumScaleFactor(0.01)
 
           }
           VStack {
-            Text("BS").font(.title2).fontWeight(.bold).foregroundColor(.white).padding(4)
-            Text("\(Unit.bs)").font(.title2).fontWeight(.bold).foregroundColor(.white).padding(4)
+            Text("BS").font(.title2).fontWeight(.bold).lineLimit(1).foregroundColor(.white).padding(4).minimumScaleFactor(0.01)
+            Text("\(Unit.bs)").font(.title2).fontWeight(.bold).lineLimit(1).foregroundColor(.white).padding(4).minimumScaleFactor(0.01)
 
           }
           VStack {
-            Text("S").font(.title2).fontWeight(.bold).foregroundColor(.white).padding(4)
-            Text("\(Unit.s)").font(.title2).fontWeight(.bold).foregroundColor(.white).padding(4)
+            Text("S").font(.title2).fontWeight(.bold).lineLimit(1).foregroundColor(.white).padding(4).minimumScaleFactor(0.01)
+            Text("\(Unit.s)").font(.title2).fontWeight(.bold).lineLimit(1).foregroundColor(.white).padding(4).minimumScaleFactor(0.01)
 
           }
           VStack {
-            Text("T").font(.title2).fontWeight(.bold).foregroundColor(.white).padding(4)
-            Text("\(Unit.t)").font(.title2).fontWeight(.bold).foregroundColor(.white).padding(4)
+            Text("T").font(.title2).fontWeight(.bold).lineLimit(1).foregroundColor(.white).padding(4).minimumScaleFactor(0.01)
+            Text("\(Unit.t)").font(.title2).fontWeight(.bold).lineLimit(1).foregroundColor(.white).padding(4).minimumScaleFactor(0.01)
 
           }
           VStack {
-            Text("W").font(.title2).fontWeight(.bold).foregroundColor(.white).padding(4)
-            Text("\(Unit.w)").font(.title2).fontWeight(.bold).foregroundColor(.white).padding(4)
+            Text("W").font(.title2).fontWeight(.bold).lineLimit(1).foregroundColor(.white).padding(4).minimumScaleFactor(0.01)
+            Text("\(Unit.w)").font(.title2).fontWeight(.bold).lineLimit(1).foregroundColor(.white).padding(4).minimumScaleFactor(0.01)
 
           }
           VStack {
-            Text("A").font(.title2).fontWeight(.bold).foregroundColor(.white).padding(4)
-            Text("\(Unit.a)").font(.title2).fontWeight(.bold).foregroundColor(.white).padding(4)
+            Text("A").font(.title2).fontWeight(.bold).lineLimit(1).foregroundColor(.white).padding(4).minimumScaleFactor(0.01)
+            Text("\(Unit.a)").font(.title2).fontWeight(.bold).lineLimit(1).foregroundColor(.white).padding(4).minimumScaleFactor(0.01)
 
           }
           VStack {
-            Text("Ld").font(.title2).fontWeight(.bold).foregroundColor(.white).padding(4)
-            Text("\(Unit.ld)").font(.title2).fontWeight(.bold).foregroundColor(.white).padding(4)
+            Text("Ld").font(.title2).fontWeight(.bold).lineLimit(1).foregroundColor(.white).padding(4)
+            Text("\(Unit.ld)").font(.title2).fontWeight(.bold).lineLimit(1).foregroundColor(.white).padding(4).minimumScaleFactor(0.01)
 
           }
           VStack {
-            Text("Sv").font(.title2).fontWeight(.bold).foregroundColor(.white).padding(4)
-            Text("\(Unit.sv)").font(.title2).fontWeight(.bold).foregroundColor(.white).padding(4)
+            Text("Sv").font(.title2).fontWeight(.bold).lineLimit(1).foregroundColor(.white).padding(4).minimumScaleFactor(0.01)
+            Text("\(Unit.sv)").font(.title2).fontWeight(.bold).lineLimit(1).foregroundColor(.white).padding(4).minimumScaleFactor(0.01)
 
           }
 
