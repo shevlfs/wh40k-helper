5a000b896ab9c7124b944f75509850c5f6fbbf41
shevlfs <shevl.fs@gmail.com>
2022-11-06 03:46:31 +0300

    Update app.py

--- server/app.py
+++ server/app.py
@@ -22,6 +22,7 @@ mail_settings = {
     "MAIL_USERNAME": mail.login,
     "MAIL_PASSWORD": mail.password
 }
+# секреты и строка для подключения к базе данных лежат в отдельном файле data.py для безопасности
 app.config.update(mail_settings)
 mail = Mail(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = \
@@ -31,14 +32,14 @@ app.config['SECRET_KEY'] = secretKey.secret
 app.config['SECURITY_PASSWORD_SALT'] = secretKey.secret2
 
 db = SQLAlchemy(app)
+# инициализация всего...
 
-
-def generate_confirmation_token(email):
+def generate_confirmation_token(email): # генератор токенов для подтверждения почты
     serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
     return serializer.dumps(email, salt=app.config['SECURITY_PASSWORD_SALT'])
 
 
-def confirm_token(token, expiration=3600):
+def confirm_token(token, expiration=3600): # функция подтверждения токена при подтверждении почты
     serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
     try:
         email = serializer.loads(
@@ -51,7 +52,7 @@ def confirm_token(token, expiration=3600):
     return email
 
 
-class UserModel(db.Model):
+class UserModel(db.Model): # класс UserModel (класс для SQLAlchemy)
     ___tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, index=True)
@@ -59,12 +60,13 @@ class UserModel(db.Model):
     verified = db.Column(db.Boolean, default=False)
     loggedin = db.Column(db.Boolean, default=False)
 
-    def get_password(self, password):
+    def get_password(self, password): # функции генерации хэша пароля
         return generate_password_hash(password)
 
-    def check_password(self, password):
+    def check_password(self, password): # функция проверки пароля на валидность
         return check_password_hash(self.password_hash, password)
 
+    # Далее идут функции требуемые Flask-Login для корректной работы (по сути, они нигде не используются, но нужны для корректной работы)
     def __get__(self, user_id):
         return self.id
 
@@ -80,12 +82,12 @@ class UserModel(db.Model):
     def is_anonymous(self):
         return False
 
-    def saveToDatabase(self):
+    def saveToDatabase(self): # функция сохранения user'a в базу данных
         db.session.add(self)
         db.session.commit()
         return "ok!"
 
-    def sendConfirmationMail(self):
+    def sendConfirmationMail(self): # функция отправки письма для подтверждения почты
         with app.app_context():
             msg = Message(subject="Email confirmation -- ArmyBuilder",
                           sender=app.config.get("MAIL_USERNAME"),
@@ -94,7 +96,7 @@ class UserModel(db.Model):
                                "94.228.195.88:5000/verification/" + generate_confirmation_token(self.username))
             mail.send(msg)
 
-    def sendChangePassMail(self):
+    def sendChangePassMail(self): # функция отправки письма для восстановления пароля
         with app.app_context():
             msg = Message(subject="Change password -- ArmyBuilder",
                           sender=app.config.get("MAIL_USERNAME"),
@@ -106,17 +108,17 @@ class UserModel(db.Model):
             mail.send(msg)
 
     @classmethod
-    def find_by_username(cls, username):
+    def find_by_username(cls, username): # метод для поиска пользователя по email
         return cls.query.filter_by(username=username).first()
 
-    def __init__(self, username, password):
+    def __init__(self, username, password): # инициализация
         self.username = username
         self.password = self.get_password(password)
         col = CollectionModel(username=username, userid=self.id)
         col.saveToDatabase()
 
 
-class ArmyModel(db.Model):
+class ArmyModel(db.Model): # класс ArmyModel (класс для SQLAlchemy)
     ___tablename__ = 'armies'
     user = db.Column(db.String(128))
     userid = db.Column(db.Integer)
@@ -124,72 +126,66 @@ class ArmyModel(db.Model):
     army = db.Column(db.JSON)
     deleted = db.Column(db.Boolean)
 
-    def saveToDatabase(self):
+    def saveToDatabase(self): # функция сохранения армии в базу данных
         db.session.add(self)
         db.session.commit()
         return "army added"
 
-    def __init__(self, username, userid, army):
+    def __init__(self, username, userid, army): # инициализация
         self.user = username
         self.userid = userid
         self.army = army
 
     @classmethod
-    def find_by_username(cls, username):
-
+    def find_by_username(cls, username): # поиск армий пользователя по email
         return cls.query.filter_by(user=username)
 
     @classmethod
-    def find_by_username_and_name(cls, username, name):
+    def find_by_username_and_name(cls, username, name): # поиск армий пользователя по email и имени самой армии
         armies = cls.query.filter_by(user=username).all()
         for army in armies:
             if army.army["name"] == name and army.army["deleted"] == False:
                 return army
 
 
-class CollectionModel(db.Model):
+class CollectionModel(db.Model): # класс CollectionModel (класс для SQLAlchemy)
     ___tablename__ = 'collections'
     user = db.Column(db.String(128))
     userid = db.Column(db.Integer)
     id = db.Column(db.Integer, primary_key=True)
     collection = db.Column(db.JSON)
 
-    def saveToDatabase(self):
+    def saveToDatabase(self): # функция сохранения коллекции в базу данных
         db.session.add(self)
         db.session.commit()
         return "collection saved"
 
-    def __init__(self, username, userid):
+    def __init__(self, username, userid): # инициализация
         self.user = username
         self.userid = userid
 
     @classmethod
-    def find_by_username(cls, username):
+    def find_by_username(cls, username): # поиск коллекции по email
         return cls.query.filter_by(user=username)
 
-
-@app.route('/', methods=['GET', 'POST'])
-def handshake():
-    return "HellO!"
-
-
 @login_manager.user_loader
-def load_user(name):
+def load_user(name): # функция flask-login для загрузки пользователя
     return UserModel.find_by_username(name)
 
 
 @login_manager.request_loader
-def request_loader(request):
+def request_loader(request): # функция необходимая flask-login для корректной обработки запросов
     email = request.form.get('name')
     if UserModel:
         return
-
     user = UserModel()
     return user
 
 
+# Далее идут функции через которые само приложение взаимодействует с бекэндом.
+
 @app.route('/registration', methods=['POST'])
-def registration():
+def registration(): # функция регистрации нового пользователя и сохранения информации о нем в базу данных
     request.get_json(force=True)
     name = request.json['name']
     if not re.match(r"[^@]+@[^@]+\.[^@]+", name):
@@ -210,7 +206,7 @@ def registration():
 
 
 @app.route('/verification/<token>')
-def verify(token):
+def verify(token): # функция подтверждения email (web)
     try:
         email = confirm_token(token)
     except:
@@ -228,7 +224,7 @@ def verify(token):
 
 
 @app.route('/login', methods=["POST"])
-def login():
+def login(): # фунция аутентификации
     request.get_json(force=True)
     name = request.json['name']
     password = request.json['password']
@@ -249,13 +245,13 @@ def login():
 
 @app.route("/whoami", methods=["GET"])
 @login_required
-def whoami():
+def whoami(): # функция для возвращения email текущего пользователя
     return current_user.username
 
 
 @app.route("/addarmy", methods=["GET", "POST"])
 @login_required
-def add_army():
+def add_army(): # функция для добавления новой армии
     request.get_json(force=True)
     army = ArmyModel(username=current_user.username, userid=current_user.id, army=request.json)
     army.saveToDatabase()
@@ -264,7 +260,7 @@ def add_army():
 
 @app.route("/savecollection", methods=["GET", "POST"])
 @login_required
-def save_collection():
+def save_collection(): # функция для сохранения коллекции
     request.get_json(force=True)
     col = CollectionModel.find_by_username(username=current_user.username).first()
     col.collection = request.json
@@ -275,7 +271,7 @@ def save_collection():
 
 @app.route("/getarmies", methods=["GET", "POST"])
 @login_required
-def get_armies():
+def get_armies(): # функция для получения всех армий пользователя
     armies = ArmyModel.find_by_username(current_user.username).all()
     ansdict = []
     for army in armies:
@@ -287,7 +283,7 @@ def get_armies():
 
 @app.route("/updatearmy", methods=["GET", "POST"])
 @login_required
-def update_army():
+def update_army(): # функция для обновления уже созданной армии
     request.get_json(force=True)
     army = ArmyModel.find_by_username_and_name(username=current_user.username, name=request.json['name'])
     army.army = request.json
@@ -298,7 +294,7 @@ def update_army():
 
 @app.route("/changearmyname", methods=["GET", "POST"])
 @login_required
-def changearmyname():
+def changearmyname(): # функция для изменения названия армии
     request.get_json(force=True)
     army = ArmyModel.find_by_username_and_name(username=current_user.username, name=request.json['oldname'])
     army.army["name"] = request.json["newname"]
@@ -309,7 +305,7 @@ def changearmyname():
 
 @app.route("/deletearmy", methods=["GET", "POST"])
 @login_required
-def deletearmy():
+def deletearmy(): # функция для удаления армии
     request.get_json(force=True)
     army = ArmyModel.find_by_username_and_name(username=current_user.username, name=request.json['name'])
     army.army["deleted"] = True
@@ -320,7 +316,7 @@ def deletearmy():
 
 @app.route("/getcollection", methods=["GET", "POST"])
 @login_required
-def get_collection():
+def get_collection(): # функция для получения коллекции пользователя
     col = CollectionModel.find_by_username(username=current_user.username).first()
     if col.collection:
         return col.collection
@@ -329,7 +325,7 @@ def get_collection():
 
 
 @app.route("/changepasswordapp", methods=["POST"])
-def changepasswordapp():
+def changepasswordapp(): # функция отправки запроса на восстановление пароля
     request.get_json(force=True)
     UserModel.sendChangePassMail(UserModel.find_by_username(username=request.json['email']))
     return "ok!"
@@ -337,7 +333,7 @@ def changepasswordapp():
 
 @app.route("/logout", methods=["GET"])
 @login_required
-def logout():
+def logout(): # функция выхода из аккаунта
     UserModel.find_by_username(current_user.username).loggedin = False
     db.session.commit()
     logout_user()
@@ -345,7 +341,7 @@ def logout():
 
 
 @app.route("/changepasswordweb/<token>", methods=["POST", "GET"])
-def changepasswordweb(token):
+def changepasswordweb(token): # функция для изменения пароля (web)
     try:
         email = confirm_token(token)
     except:
@@ -363,4 +359,4 @@ def changepasswordweb(token):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000) # запуск самого бэкенда
